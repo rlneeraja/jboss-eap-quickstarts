4dcc18023781385af07c716ef1f017d11d1d6195
rlneeraja <rlneeraja@yahoo.com>
2018-10-03 17:53:50 -0400

    fixed 5

--- spring-resteasy/src/main/java/org/jboss/as/quickstarts/resteasyspring/HelloSpringResource.java
+++ spring-resteasy/src/main/java/org/jboss/as/quickstarts/resteasyspring/HelloSpringResource.java
@@ -75,7 +75,7 @@ public class HelloSpringResource {
     @GET
     @Path("fhir")
     @Produces("application/json")
-    public Response sayHello(@QueryParam("name") String name) {
+    public Response sayHellofhir(@QueryParam("name") String name) {
         String greetingMsg = greetingBean.greet("Testing Team 6");
         RestTemplate restTemplate = new RestTemplate();
         greetingMsg  = restTemplate.getForObject("http://object.datalake.services-dev.cdc.gov/api/1.0/deathonfhir/team6/find?from=0&order=1&size=-1");   
